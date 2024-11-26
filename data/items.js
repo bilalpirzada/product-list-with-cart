6bac4c3dd2e9767c45950a6d3d1b1f1794e36a7a
Muhammad Bilal <62239033+bilalpirzada@users.noreply.github.com>
2024-11-26 14:18:18 +0500

    build layout with html css

---
+++ data/items.js
@@ -0,0 +1,56 @@
+export const items=[
+  {
+  title:'Waffle with Berries',
+  subtitle:'Waffle',
+  image:'../assets/images/image-waffle-desktop.jpg',
+  priceCents:650
+  },
+  {
+    title:'Vanilla Bean Crème Brûlée',
+    subtitle:'Crème Brûlée',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:700
+  },
+  {
+    title:'Macaron Mix of Five',
+    subtitle:'Macaron',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:800
+  },
+  {
+    title:'Classic Tiramisu',
+    subtitle:'Tiramisu',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:550
+  },
+  {
+    title:'Pistachio Baklava',
+    subtitle:'Baklava',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:400
+  },
+  {
+    title:'Lemon Meringue Pie',
+    subtitle:'Pie',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:500
+  },
+  {
+    title:'Red Velvet Cake',
+    subtitle:'Cake',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:450
+  },
+  {
+    title:'Salted Caramel Brownie',
+    subtitle:'Brownie',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:450
+  },
+  {
+    title:'Vanilla Panna Cotta',
+    subtitle:'Panna Cotta',
+    image:'../assets/images/image-waffle-desktop.jpg',
+    priceCents:650
+  },
+];
